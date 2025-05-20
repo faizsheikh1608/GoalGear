48526a3189b67de54469067d19023f39a8bf7cba
Faiz Sheikh <sheikhfaiz7861@gmail.com>
2025-05-21 01:46:05 +0530

    final

--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -1,86 +1,88 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const {Schema} = mongoose;
-
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    productName : {
-        type : String,
-        required : true,
-        trim : true,
+  productName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  description: {
+    descriptionHeading: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    stock : {
-        type : Number,
-        trim : true,
-        required : true,
+    descriptionData: {
+      type: [String],
     },
-    description :
-        {
-            descriptionHeading : {
-                type : String,
-                required : true,
-                trim : true,
-            },
-            descriptionData :{
-                type : [String],    
-            }
-        }
-    ,
-    price : {
-        type : Number,
-        required : true,
-        trim : true,
-    },
-    color : [
-        {
-            colorName: {
-                type: String,
-                required: true,
-            },
-            mainImageUrl: {
-                type: String,
-                required: true,
-                validate(value) {
-                    if (!validator.isURL(value)) {
-                        throw new Error("Main image URL must be valid");
-                    }
-                },
-            },
-            hoverImageUrl: {
-                type: String,
-                required: true,
-                validate(value) {
-                    if (!validator.isURL(value)) {
-                        throw new Error("Hover image URL must be valid");
-                    }
-                },
-            },
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  color: [
+    {
+      colorName: {
+        type: String,
+        required: true,
+      },
+      mainImageUrl: {
+        type: String,
+        required: true,
+        validate(value) {
+          if (!validator.isURL(value)) {
+            throw new Error('Main image URL must be valid');
+          }
         },
-    ],
-    category : {
-        type : String,
-        enum : {
-            values : ['Shoes','Socks',"T-Shirts",'Shorts','Assecceires','Tracksuit','Football'],
-            message : `{VALUE} is not a valid role`,
+      },
+      hoverImageUrl: {
+        type: String,
+        required: true,
+        validate(value) {
+          if (!validator.isURL(value)) {
+            throw new Error('Hover image URL must be valid');
+          }
         },
-        trim : true,
-        required : true,
+      },
     },
-    size : {
-        type : [String],
-        enum : {
-            values : ['S','M',"L",'XL','3','4'.'5','6','7','8','9','10'],
-            message : `{VALUE} is not valid`,
-        },
-       
-    }       
+  ],
+  category: {
+    type: String,
+    enum: {
+      values: [
+        'Shoes',
+        'Socks',
+        'T-Shirts',
+        'Shorts',
+        'Assecceires',
+        'Tracksuit',
+        'Football',
+      ],
+      message: `{VALUE} is not a valid role`,
+    },
+    trim: true,
+    required: true,
+  },
+  size: {
+    type: [String],
+    enum: {
+      values: ['S', 'M', 'L', 'XL', '3', '4', '5', '6', '7', '8', '9', '10'],
+      message: `{VALUE} is not valid`,
+    },
+  },
+});
 
-})
-
-
-const Product = mongoose.model("Product" , productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = {
-    Product
-}
+  Product,
+};
