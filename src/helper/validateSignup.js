064b25d707e32e7490faaf473495071cf1a453ab
Faiz Sheikh <sheikhfaiz7861@gmail.com>
2025-06-14 14:04:36 +0530

    fis

--- src/helper/validateSignup.js
+++ src/helper/validateSignup.js
@@ -1,22 +1,28 @@
-const validator = require("validator")
+const validator = require('validator');
 
-const validateSignup = function(req){
-    const {firstName,lastName,emailId,age,gender} = req.body;
+const validateSignup = function (req) {
+  const { firstName, lastName, emailId, age, gender } = req.body;
 
+  if (!firstName || !validator.isAlpha(firstName, 'en-US', { ignore: ' -' })) {
+    throw new Error('First name must contain only alphabets.');
+  }
 
-    if(!validator.isAlpha(firstName) || !validator.isAlpha(lastName) || !validator.isAlpha(gender)){
-        throw new Error("Please Enter Only Alphabates in (firstNAme,lastName,gender).....")        
-    }
+  if (!lastName || !validator.isAlpha(lastName, 'en-US', { ignore: ' -' })) {
+    throw new Error('Last name must contain only alphabets.');
+  }
 
-    if(!validator.isEmail(emailId)){
-        throw new Error("Please Enter Valid Email Id.....")
-    }
+  if (!emailId || !validator.isEmail(emailId)) {
+    throw new Error('Invalid email address.');
+  }
 
-    if(!validator.isInt(age.toString())){
-        throw new Error("Please Enter a Numeric value in age....")
-    }
+  if (!age || !validator.isInt(age.toString(), { min: 1, max: 150 })) {
+    throw new Error('Age must be a valid number between 1 and 150.');
+  }
 
-    
-}
+  const validGenders = ['male', 'female', 'other'];
+  if (!gender || !validGenders.includes(gender.toLowerCase())) {
+    throw new Error('Gender must be one of male, female, or other.');
+  }
+};
 
 module.exports = validateSignup;
